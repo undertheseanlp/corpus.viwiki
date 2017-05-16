06e32cb0e033bed8f5f8f774d9d4212bcaec7796
Vu Anh <brother.rain.1024@gmail.com>
2017-05-16 17:06:52 +0700

    update data

--- bot.bat
+++ bot.bat
@@ -1,6 +1,7 @@
 set PYTHONIOENCODING=utf-8
 call activate corpus.viwiki
 python script.py
+python report\report.py
 git add -A
 git commit -m "update data"
 git push origin master
--- report/report.py
+++ report/report.py
@@ -3,7 +3,7 @@ from jinja2 import Template
 from os.path import dirname, join
 
 current_directory = dirname(__file__)
-template = open(join(current_directory, "report.template")).read()
+template = open(join(current_directory, "report.template"), encoding="utf-8").read()
 template = Template(template)
 num_documents = len(listdir(join(dirname(current_directory), "viwiki")))
 content = template.render(num_documents=num_documents)
